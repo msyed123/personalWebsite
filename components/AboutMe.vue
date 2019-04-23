8c4c81d5eb0a8c76a5c9c20fc400457d74abbf14
msyed123 <mamoonhsyed@gmail.com>
2019-04-22 22:25:03 -0400

    Initial commit.  A basic scaffold of a very simple Nuxt.JS webapp. It will be my website one day.

---
+++ components/AboutMe.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <div>
+            <h1>
+                About me
+            </h1>
+            <p>
+                I am just a simple man trying to figure out how to make things go up.
+            </p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    h1{
+        padding-top: 20px;
+        padding-left: 20px;
+        text-align: left;
+        font-family: monaco, monospace;
+    }
+    p {
+        padding-top: 20px;
+        padding-left: 20px;
+        text-align: left;
+        font-family: monaco, monospace;
+    }
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <AboutMe />
+</template>
+
+<script>
+import AboutMe from '~/components/AboutMe.vue'
+export default {
+    components: {
+        AboutMe
+    }
+}
+</script>
+
+<style>
+
+</style>
