68779fb73372889254c5a2e8b9927665bd51bbb8
msyed123 <mamoonhsyed@gmail.com>
2019-04-24 23:23:06 -0400

    Minor change to the about me page.

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -6,6 +6,7 @@
             </h1>
             <p>
                 I am just a simple man trying to figure out how to make things go up.
+                I am currently studying aerospace engineering at the Florida Institute of Technology where I am currently a sophomore pursuing a bachelors.
             </p>
         </div>
     </section>
@@ -27,6 +28,7 @@ export default {
     p {
         padding-top: 20px;
         padding-left: 20px;
+        padding-right: 20px;
         text-align: left;
         font-family: monaco, monospace;
     }
