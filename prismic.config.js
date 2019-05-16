4f95360eff495befd933a00e0bd009541373848b
msyed123 <mamoonhsyed@gmail.com>
2019-05-16 02:28:35 -0400

    Delete prismic.config.js
    API data shouldn't be exposed like this.

--- prismic.config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    apiEndpoint: "https://msyed.cdn.prismic.io/api/v2",
-    accessToken: "MC5YTnpfUXhBQUFIS3NuM0dD.77-9OO-_vXHvv71v77-9OO-_vTnvv73vv70377-977-9HH0WAnXvv71haWXvv71977-977-977-9Qe-_ve-_vQ"
-}
