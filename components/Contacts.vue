c414204519f4844a90bf7fe8d7845a95c89509cf
msyed123 <mamoonhsyed@gmail.com>
2020-12-06 04:36:39 -0500

    Update Contacts.vue

--- components/Contacts.vue
+++ components/Contacts.vue
@@ -50,7 +50,7 @@ export default {
 
     methods: {
         async getContacts() {
-            const api = await Prismic.getApi(env.PRISMIC_ENDPOINT, {accessToken: env.PRISMIC_TOKEN});
+            const api = await Prismic.getApi(process.env.PRISMIC_ENDPOINT, {accessToken: process.env.PRISMIC_TOKEN});
 
             const response = await api.query(
                 Prismic.Predicates.at("document.type", "contact"),
