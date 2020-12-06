96eea7243f00b41357a54d356303acd306cc4d83
msyed123 <mamoonhsyed@gmail.com>
2020-12-06 03:03:07 -0500

    Turns out that I'm having an auth error.

--- components/Projects.vue
+++ components/Projects.vue
@@ -65,7 +65,7 @@ export default {
 
     methods: {
         async getProjects() {
-            const api = await Prismic.getApi("https://msyed.cdn.prismic.io/api/v2", {accessToken: process.env.PRISMIC_TOKEN});
+            const api = await Prismic.getApi(process.env.PRISMIC_ENDPOINT, {accessToken: process.env.PRISMIC_TOKEN});
             const response = await api.query(
                 Prismic.Predicates.at("document.type", "project"),
                 { orderings : '[my.project.priority]'}
