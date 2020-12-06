eae4b8e9499853738c9d24870d6ec34c7ed031bc
msyed123 <mamoonhsyed@gmail.com>
2020-12-06 04:37:38 -0500

    Update Projects.vue

--- components/Projects.vue
+++ components/Projects.vue
@@ -65,7 +65,7 @@ export default {
 
     methods: {
         async getProjects() {
-            const api = await Prismic.getApi(env.PRISMIC_ENDPOINT, {accessToken: env.PRISMIC_TOKEN});
+            const api = await Prismic.getApi(process.env.PRISMIC_ENDPOINT, {accessToken: process.env.PRISMIC_TOKEN});
             const response = await api.query(
                 Prismic.Predicates.at("document.type", "project"),
                 { orderings : '[my.project.priority]'}
