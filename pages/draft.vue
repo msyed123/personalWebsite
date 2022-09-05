1c1daa5fa58f5963be368e690e3886f431db7ffa
Mamoon Syed <mamoonhsyed@gmail.com>
2022-09-05 17:15:21 -0400

    For the draft.

---
+++ pages/draft.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <div>
+            <br />
+            <DraftOrder />
+            <br />
+        </div>
+    </section>
+</template>
+
+<script>
+import DraftOrder from '~/components/DraftOrder.vue'
+export default {
+    components: {
+        DraftOrder
+    }
+}
+</script>
+
+<style>
+    h1{
+        padding-top: 20px;
+        padding-left: 20px;
+        text-align: left;
+        font-family: monaco, monospace;
+    }
+    p {
+        padding-top: 20px;
+        padding-left: 20px;
+        padding-right: 20px;
+        text-align: left;
+        font-family: monaco, monospace;
+    }
+</style>
