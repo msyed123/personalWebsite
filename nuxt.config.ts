325c5b9aebfcbf26b2c63b6db0ddc920c1309ff6
msyed123 <mamoonhsyed@gmail.com>
2026-03-26 10:44:36 -0400

    More attempts to fix the Heroku deploy.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  experimental: {
+    asyncContext: true
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/prismic'
